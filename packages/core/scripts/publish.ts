da92115dc62efb8cbf0fd3257bfd9e82dc58db15
berlin <1580940252@qq.com>
2022-12-01 19:50:19 +0800

    style: remove

--- packages/core/scripts/publish.ts
+++ packages/core/scripts/publish.ts
@@ -3,7 +3,6 @@ import path from "path";
 import consola from "consola";
 import { version } from "../package.json";
 
-execSync("npm run update", { stdio: "inherit" });
 execSync("npm run build", { stdio: "inherit" });
 
 let command = "npm publish --access public";
