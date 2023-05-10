40ea8edb82c92ed45bb1f9583cb07c2099f6e7f2
Riri <Daydreamerriri@outlook.com>
2023-05-10 13:54:18 +0800

    chore(core): type err

--- packages/core/hooks/useFps/index.ts
+++ packages/core/hooks/useFps/index.ts
@@ -30,8 +30,6 @@ function useFps(options?: UseFpsOptions): number {
   return fps;
 }
 
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-expect-error
 // eslint-disable-next-line unused-imports/no-unused-vars
 const useFpsMock = (options?: UseFpsOptions) => 0;
 
