8c95b4cfb24cd1500c9558cbd628c1c57b73c81c
berlin <1580940252@qq.com>
2023-05-16 21:01:45 +0800

    feat(core): add useDoubleClick

---
+++ packages/core/hooks/useDoubleClick/demo.tsx
@@ -0,0 +1,23 @@
+import { useDoubleClick } from "@reactuses/core";
+import { useRef, useState } from "react";
+
+export default () => {
+  const element = useRef<HTMLButtonElement>(null);
+  const [text,setText] = useState('no click')
+
+  useDoubleClick({
+    target: element,
+    onSingleClick: () => {
+      setText('single click')
+    },
+    onDoubleClick: () => {
+      setText('double click')
+    }
+  })
+  return (
+    <div>
+      <button ref={element}>Click Me</button>
+      <p>{text}</p>
+    </div>
+  );
+};
