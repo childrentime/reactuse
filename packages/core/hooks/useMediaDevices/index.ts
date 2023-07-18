031575143fd6fe7946b775dc086778b44f965a8c
Berlin <58261676+childrentime@users.noreply.github.com>
2023-07-18 20:27:28 +0800

    feat(core): useMediaDevices: support request permission (#42)

--- packages/core/hooks/useMediaDevices/index.ts
+++ packages/core/hooks/useMediaDevices/index.ts
@@ -1,8 +1,26 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
+import useSupported from "../useSupported";
 import { off, on } from "../utils/browser";
 import { isNavigator, noop } from "../utils/is";
 
-function useMediaDevices() {
+export interface UseMediaDeviceOptions {
+  /**
+   * Request for permissions immediately if it's not granted,
+   * otherwise label and deviceIds could be empty
+   *
+   * @default false
+   */
+  requestPermissions?: boolean;
+  /**
+   * Request for types of media permissions
+   *
+   * @default { audio: true, video: true }
+   */
+  constraints?: MediaStreamConstraints;
+}
+function useMediaDevices(options: UseMediaDeviceOptions = {}) {
+  const { requestPermissions, constraints = { audio: true, video: true } }
+    = options;
   const [state, setState] = useState<{
     devices: {
       deviceId: string;
@@ -11,41 +29,78 @@ function useMediaDevices() {
       label: string;
     }[];
   }>({ devices: [] });
+  const isSupported = useSupported(
+    () =>
+      navigator
+      && navigator.mediaDevices
+      && navigator.mediaDevices.enumerateDevices,
+  );
+  const permissionGranted = useRef(false);
+  const stream = useRef<MediaStream | null>(null);
+
+  const onChange = useCallback(() => {
+    navigator.mediaDevices
+      .enumerateDevices()
+      .then((devices) => {
+        if (stream.current) {
+          stream.current.getTracks().forEach(t => t.stop());
+          stream.current = null;
+        }
+        setState({
+          devices: devices.map(({ deviceId, groupId, kind, label }) => ({
+            deviceId,
+            groupId,
+            kind,
+            label,
+          })),
+        });
+      })
+      .catch(noop);
+  }, []);
+
+  const ensurePermissions = useCallback(async () => {
+    if (!isSupported) {
+      return false;
+    }
+    if (permissionGranted.current) {
+      return true;
+    }
+    const permissionStatus = await navigator!.permissions.query({
+      name: "camera",
+    } as unknown as PermissionDescriptor);
+
+    if (permissionStatus.state !== "granted") {
+      stream.current = await navigator!.mediaDevices.getUserMedia(constraints);
+      onChange();
+      permissionGranted.current = true;
+    }
+    else {
+      permissionGranted.current = false;
+    }
+
+    return permissionGranted.current;
+  }, [onChange, isSupported, constraints]);
 
   useEffect(() => {
-    let mounted = true;
-
-    const onChange = () => {
-      navigator.mediaDevices
-        .enumerateDevices()
-        .then((devices) => {
-          if (mounted) {
-            setState({
-              devices: devices.map(({ deviceId, groupId, kind, label }) => ({
-                deviceId,
-                groupId,
-                kind,
-                label,
-              })),
-            });
-          }
-        })
-        .catch(noop);
-    };
+    if (!isSupported) {
+      return;
+    }
+    if (requestPermissions) {
+      ensurePermissions();
+    }
 
     on(navigator.mediaDevices, "devicechange", onChange);
     onChange();
 
     return () => {
-      mounted = false;
       off(navigator.mediaDevices, "devicechange", onChange);
     };
-  }, []);
+  }, [onChange, isSupported, requestPermissions, ensurePermissions]);
 
-  return state;
+  return [state, ensurePermissions] as const;
 }
 
-const useMediaDevicesMock = () => ({ devices: [] });
+const useMediaDevicesMock = () => [{ devices: [] }, () => {}];
 
 export default isNavigator && !!navigator.mediaDevices
   ? useMediaDevices
