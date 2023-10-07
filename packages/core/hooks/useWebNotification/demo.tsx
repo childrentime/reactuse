59fac9b040eb805d75fb02bcde26fb6178bfca28
berlin <1580940252@qq.com>
2023-10-07 14:58:00 +0800

    feat: add useWebNotification

---
+++ packages/core/hooks/useWebNotification/demo.tsx
@@ -0,0 +1,24 @@
+import useWebNotification from ".";
+
+export default function Demo() {
+  const [isSupported, show, close] = useWebNotification();
+  return (
+    <div>
+      <p>Supported: {JSON.stringify(isSupported)}</p>
+      {isSupported ? (
+        <div>
+          <button
+            onClick={() => {
+              show("Hello, world from ReactUse!");
+            }}
+          >
+            Show Notification
+          </button>
+          <button onClick={close}>Close</button>
+        </div>
+      ) : (
+        <div>The Notification Web API is not supported in your browser.</div>
+      )}
+    </div>
+  );
+}
---
+++ packages/core/hooks/useWebNotification/index.ts
@@ -0,0 +1,60 @@
+import { useCallback, useEffect, useRef } from "react";
+import useSupported from "../useSupported";
+import useUnmount from "../useUnmount";
+import { defaultOptions } from "../utils/defaults";
+
+export default function useWebNotification(requestPermissions = false) {
+  const isSupported = useSupported(() => !!window && "Notification" in window);
+  const permissionGranted = useRef(
+    isSupported
+      && "permission" in Notification
+      && Notification.permission === "granted",
+  );
+
+  const notificationRef = useRef<Notification | null>(null);
+
+  const ensurePermissions = useCallback(async () => {
+    if (!isSupported)
+      return;
+
+    if (!permissionGranted.current && Notification.permission !== "denied") {
+      const result = await Notification.requestPermission();
+      if (result === "granted")
+        permissionGranted.current = true;
+    }
+
+    return permissionGranted.current;
+  }, [isSupported]);
+
+  const show = (
+    title: string,
+    options: NotificationOptions = defaultOptions,
+  ) => {
+    // If either the browser does not support notifications or the user has
+    // not granted permission, do nothing:
+    if (!isSupported && !permissionGranted.current) {
+      return;
+    }
+
+    notificationRef.current = new Notification(title || "", options);
+    return notificationRef.current;
+  };
+
+  const close = useCallback((): void => {
+    if (notificationRef.current) {
+      notificationRef.current.close();
+    }
+
+    notificationRef.current = null;
+  }, []);
+
+  useEffect(() => {
+    if (requestPermissions) {
+      ensurePermissions();
+    }
+  }, [requestPermissions, ensurePermissions]);
+
+  useUnmount(close);
+
+  return [isSupported, show, close] as const;
+}
