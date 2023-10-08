e5c0375d4359ff0006aaf8ce389a9baaffb373ea
berlin <1580940252@qq.com>
2023-10-08 20:39:02 +0800

    feat: add useLocationSelector

---
+++ packages/core/hooks/useLocationSelector/index.spec.ts
@@ -0,0 +1,7 @@
+import useLocationSelector from ".";
+
+describe("useLocationSelector", () => {
+  it("should be defined", () => {
+    expect(useLocationSelector).toBeDefined();
+  });
+});
---
+++ packages/core/hooks/useLocationSelector/index.ts
@@ -0,0 +1,25 @@
+import { useSyncExternalStore } from "react";
+
+function subscribe(callback: () => void) {
+  window.addEventListener("popstate", callback);
+  window.addEventListener("hashchange", callback);
+  return () => {
+    window.removeEventListener("popstate", callback);
+    window.removeEventListener("hashchange", callback);
+  };
+}
+
+export default function useLocationSelector<R>(
+  selector: (location: Location) => R,
+  /**
+   * @description server fallback
+   * @default undefined
+   */
+  fallback?: R
+) {
+  return useSyncExternalStore<R | undefined>(
+    subscribe,
+    () => selector(location),
+    () => fallback
+  );
+}
