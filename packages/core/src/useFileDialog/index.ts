cd380992fe81aca037dedf87370d29c8feb052e4
Berlin <1580940252@qq.com>
2024-04-29 11:43:43 +0800

    feat(useFileDialog): add open files return

--- packages/core/src/useFileDialog/index.ts
+++ packages/core/src/useFileDialog/index.ts
@@ -9,14 +9,11 @@ const DEFAULT_OPTIONS: UseFileDialogOptions = {
 
 export const useFileDialog: UseFileDialog = (
   options: UseFileDialogOptions = defaultOptions,
-): readonly [
-    FileList | null,
-    (localOptions?: Partial<UseFileDialogOptions>) => void,
-    () => void,
-  ] => {
+) => {
   const [files, setFiles] = useState<FileList | null>(null);
-
   const inputRef = useRef<HTMLInputElement>();
+  const fileOpenPromiseRef = useRef<Promise<FileList | null> | null>(null);
+  const resolveFileOpenPromiseRef = useRef<(value: FileList | null) => void>();
   const initFn = useCallback(() => {
     if (typeof document === "undefined") {
       return undefined;
@@ -27,12 +24,13 @@ export const useFileDialog: UseFileDialog = (
     input.onchange = (event: Event) => {
       const result = event.target as HTMLInputElement;
       setFiles(result.files);
+      resolveFileOpenPromiseRef.current?.(result.files);
     };
     return input;
   }, []);
   inputRef.current = initFn();
 
-  const open = (localOptions?: Partial<UseFileDialogOptions>) => {
+  const open = async (localOptions?: Partial<UseFileDialogOptions>) => {
     if (!inputRef.current) {
       return;
     }
@@ -46,11 +44,16 @@ export const useFileDialog: UseFileDialog = (
     inputRef.current.accept = _options.accept!;
     inputRef.current.capture = _options.capture!;
 
+    fileOpenPromiseRef.current = new Promise((resolve) => {
+      resolveFileOpenPromiseRef.current = resolve;
+    });
     inputRef.current.click();
+    return fileOpenPromiseRef.current;
   };
 
   const reset = () => {
     setFiles(null);
+    resolveFileOpenPromiseRef.current?.(null);
     if (inputRef.current) {
       inputRef.current.value = "";
     }
