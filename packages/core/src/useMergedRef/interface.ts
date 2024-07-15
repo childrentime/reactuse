0058f9620ae9c872e8bd70be196f3e2a2187791b
Berlin <1580940252@qq.com>
2024-07-15 16:24:55 +0800

    feat: add useMergedRefs

---
+++ packages/core/src/useMergedRef/interface.ts
@@ -0,0 +1,8 @@
+import type { Ref } from "react";
+
+/**
+ * @title useMergedRef
+ */
+export type UseMergedRef = <T>(...refs: PossibleRef<T>[]) => (node: T | null) => void;
+
+export type PossibleRef<T> = Ref<T> | undefined;
