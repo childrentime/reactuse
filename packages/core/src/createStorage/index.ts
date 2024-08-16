790eae2758a0d277a32d2f9a572644e8c5df0b75
Berlin <1580940252@qq.com>
2024-08-16 12:03:31 +0800

    style: lint

--- packages/core/src/createStorage/index.ts
+++ packages/core/src/createStorage/index.ts
@@ -1,22 +1,22 @@
-import { Dispatch, SetStateAction, useEffect } from "react";
-import { useState } from "react";
-import { isBrowser, isFunction } from "../utils/is";
-import { guessSerializerType } from "../utils/serializer";
-import { useEvent } from "../useEvent";
-import { defaultOnError, defaultOptions } from "../utils/defaults";
-import { useDeepCompareEffect } from "../useDeepCompareEffect";
+import type { Dispatch, SetStateAction } from 'react'
+import { useEffect, useState } from 'react'
+import { isBrowser, isFunction } from '../utils/is'
+import { guessSerializerType } from '../utils/serializer'
+import { useEvent } from '../useEvent'
+import { defaultOnError, defaultOptions } from '../utils/defaults'
+import { useDeepCompareEffect } from '../useDeepCompareEffect'
 
 export interface Serializer<T> {
-  read: (raw: string) => T;
-  write: (value: T) => string;
+  read: (raw: string) => T
+  write: (value: T) => string
 }
 
 export const StorageSerializers: Record<
-  "boolean" | "object" | "number" | "any" | "string" | "map" | "set" | "date",
+  'boolean' | 'object' | 'number' | 'any' | 'string' | 'map' | 'set' | 'date',
   Serializer<any>
 > = {
   boolean: {
-    read: (v: any) => v === "true",
+    read: (v: any) => v === 'true',
     write: (v: any) => String(v),
   },
   object: {
@@ -48,165 +48,173 @@ export const StorageSerializers: Record<
     read: (v: any) => new Date(v),
     write: (v: any) => v.toISOString(),
   },
-};
+}
 
 export interface UseStorageOptions<T> {
   /**
    * @en Custom data serialization
    * @zh 自定义数据序列化
    */
-  serializer?: Serializer<T>;
+  serializer?: Serializer<T>
   /**
    * @en On error callback
    * @zh 错误回调
    * @defaultValue `console.error`
    */
-  onError?: (error: unknown) => void;
+  onError?: (error: unknown) => void
   /**
    * @en set to storage when nodata in first mount
    * @zh 首次挂载时没有数据时设置到 storage
    * @deprecated
    */
-  effectStorageValue?: T | (() => T);
+  effectStorageValue?: T | (() => T)
   /**
    * @en set to storage when nodata in first mount
    * @zh 首次挂载时没有数据时设置到 storage
    */
-  mountStorageValue?: T | (() => T);
+  mountStorageValue?: T | (() => T)
   /**
    * @en listen to storage changes
    * @zh 监听 storage 变化
    * @defaultValue `true`
    */
-  listenToStorageChanges?: boolean;
+  listenToStorageChanges?: boolean
 }
 function getInitialState(
   key: string,
   defaultValue?: any,
   storage?: Storage,
   serializer?: Serializer<any>,
-  onError?: (error: unknown) => void
+  onError?: (error: unknown) => void,
 ) {
   // Prevent a React hydration mismatch when a default value is provided.
   if (defaultValue !== undefined) {
-    return defaultValue;
+    return defaultValue
   }
 
   if (isBrowser) {
     try {
-      const raw = storage?.getItem(key);
+      const raw = storage?.getItem(key)
       if (raw !== undefined && raw !== null) {
-        return serializer?.read(raw);
+        return serializer?.read(raw)
       }
-      return null;
-    } catch (error) {
-      onError?.(error);
+      return null
+    }
+    catch (error) {
+      onError?.(error)
     }
   }
 
   // A default value has not been provided, and you are rendering on the server, warn of a possible hydration mismatch when defaulting to false.
-  if (process.env.NODE_ENV !== "production") {
+  if (process.env.NODE_ENV !== 'production') {
     console.warn(
-      "`createStorage` When server side rendering, defaultValue should be defined to prevent a hydration mismatches."
-    );
+      '`createStorage` When server side rendering, defaultValue should be defined to prevent a hydration mismatches.',
+    )
   }
 
-  return null;
+  return null
 }
 
 export default function useStorage<
-  T extends string | number | boolean | object | null
+  T extends string | number | boolean | object | null,
 >(
   key: string,
   defaultValue?: T,
   getStorage: () => Storage | undefined = () =>
     isBrowser ? sessionStorage : undefined,
-  options: UseStorageOptions<T> = defaultOptions
+  options: UseStorageOptions<T> = defaultOptions,
 ) {
-  let storage: Storage | undefined;
+  let storage: Storage | undefined
   const {
     onError = defaultOnError,
     effectStorageValue,
     mountStorageValue,
     listenToStorageChanges = true,
-  } = options;
-  const storageValue = mountStorageValue ?? effectStorageValue;
+  } = options
+  const storageValue = mountStorageValue ?? effectStorageValue
 
   try {
-    storage = getStorage();
-  } catch (err) {
-    onError(err);
+    storage = getStorage()
+  }
+  catch (err) {
+    onError(err)
   }
 
-  const type = guessSerializerType<T | undefined>(defaultValue);
-  const serializer = options.serializer ?? StorageSerializers[type];
+  const type = guessSerializerType<T | undefined>(defaultValue)
+  const serializer = options.serializer ?? StorageSerializers[type]
 
   const [state, setState] = useState<T | null>(
-    getInitialState(key, defaultValue, storage, serializer, onError)
-  );
+    getInitialState(key, defaultValue, storage, serializer, onError),
+  )
 
   useDeepCompareEffect(() => {
-    const data =
-      (storageValue
+    const data
+      = (storageValue
         ? isFunction(storageValue)
           ? storageValue()
           : storageValue
-        : defaultValue) ?? null;
+        : defaultValue) ?? null
 
     const getStoredValue = () => {
       try {
-        const raw = storage?.getItem(key);
+        const raw = storage?.getItem(key)
         if (raw !== undefined && raw !== null) {
-          return serializer.read(raw);
-        } else {
-          storage?.setItem(key, serializer.write(data));
-          return data;
+          return serializer.read(raw)
         }
-      } catch (e) {
-        onError(e);
+        else {
+          storage?.setItem(key, serializer.write(data))
+          return data
+        }
       }
-    };
-
-    setState(getStoredValue());
-  }, [key, serializer, storage, onError, storageValue]);
-
-  const updateState: Dispatch<SetStateAction<T | null>> = useEvent(
-    (valOrFunc) => {
-      const currentState = isFunction(valOrFunc) ? valOrFunc(state) : valOrFunc;
-      setState(currentState);
-
-      if (currentState === null) {
-        storage?.removeItem(key);
-      } else {
-        try {
-          storage?.setItem(key, serializer.write(currentState));
-        } catch (e) {
-          onError(e);
-        }
+      catch (e) {
+        onError(e)
       }
     }
-  );
+
+    setState(getStoredValue())
+  }, [key, serializer, storage, onError, storageValue])
+
+  const updateState: Dispatch<SetStateAction<T | null>> = useEvent(
+    valOrFunc => {
+      const currentState = isFunction(valOrFunc) ? valOrFunc(state) : valOrFunc
+      setState(currentState)
+
+      if (currentState === null) {
+        storage?.removeItem(key)
+      }
+      else {
+        try {
+          storage?.setItem(key, serializer.write(currentState))
+        }
+        catch (e) {
+          onError(e)
+        }
+      }
+    },
+  )
 
   const listener = useEvent(() => {
     try {
-      const raw = storage?.getItem(key);
+      const raw = storage?.getItem(key)
       if (raw !== undefined && raw !== null) {
-        updateState(serializer.read(raw));
-      }else {
-        updateState(null);
+        updateState(serializer.read(raw))
       }
-    } catch (e) {
-      onError(e);
+      else {
+        updateState(null)
+      }
     }
-  });
+    catch (e) {
+      onError(e)
+    }
+  })
 
   useEffect(() => {
     if (listenToStorageChanges) {
-      window.addEventListener("storage", listener);
-      return () => window.removeEventListener("storage", listener);
+      window.addEventListener('storage', listener)
+      return () => window.removeEventListener('storage', listener)
     }
-    return () => {};
-  }, [listenToStorageChanges, listener]);
+    return () => {}
+  }, [listenToStorageChanges, listener])
 
-  return [state, updateState] as const;
+  return [state, updateState] as const
 }
