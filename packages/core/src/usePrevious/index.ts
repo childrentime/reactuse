81e5ad5a9ef96072920e7df1764c603382511ece
Jakob Rössner <jakob.roessner@outlook.de>
2024-12-07 03:23:03 +1100

    fix: optimize usePrevious (#116)

--- packages/core/src/usePrevious/index.ts
+++ packages/core/src/usePrevious/index.ts
@@ -1,11 +1,17 @@
-import { useEffect, useRef } from 'react'
+import { useState } from 'react'
 
-export function usePrevious<T>(state: T): T | undefined {
-  const ref = useRef<T>()
+// Following these issues I think this is the best way to implement usePrevious:
+// https://github.com/childrentime/reactuse/issues/115
+// https://github.com/streamich/react-use/issues/2605
+// https://github.com/alibaba/hooks/issues/2162
+export function usePrevious<T>(value: T): T | undefined {
+  const [current, setCurrent] = useState<T>(value)
+  const [previous, setPrevious] = useState<T>()
 
-  useEffect(() => {
-    ref.current = state
-  })
+  if (value !== current) {
+    setPrevious(current)
+    setCurrent(value)
+  }
 
-  return ref.current
+  return previous
 }
