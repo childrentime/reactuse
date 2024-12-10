3255abd7f0bc189743cbfb7b7343b5384ac2c78b
Jakob Rössner <jakob.roessner@outlook.de>
2024-12-11 01:02:45 +1100

    fix: docusaurus internal code formatting
    - Fix issue described in: https://github.com/FormidableLabs/react-live/issues/403
    - Reverted most of the changes of a1f64f7fb67dcda40c874a66b5a77377e8fccd85
    - Added a new React 19 hint to the use hook

---
+++ packages/website-docusaurus/src/theme/Playground/index.tsx
@@ -0,0 +1,141 @@
+import React from "react";
+import clsx from "clsx";
+import useIsBrowser from "@docusaurus/useIsBrowser";
+import { LiveProvider, LiveEditor, LiveError, LivePreview } from "react-live";
+import Translate from "@docusaurus/Translate";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import BrowserOnly from "@docusaurus/BrowserOnly";
+import {
+  ErrorBoundaryErrorMessageFallback,
+  usePrismTheme,
+} from "@docusaurus/theme-common";
+import ErrorBoundary from "@docusaurus/ErrorBoundary";
+
+import type { Props } from "@theme/Playground";
+import type { ThemeConfig } from "@docusaurus/theme-live-codeblock";
+
+import styles from "./styles.module.css";
+
+function Header({ children }: { children: React.ReactNode }) {
+  return <div className={clsx(styles.playgroundHeader)}>{children}</div>;
+}
+
+function LivePreviewLoader() {
+  // Is it worth improving/translating?
+  // eslint-disable-next-line @docusaurus/no-untranslated-text
+  return <div>Loading...</div>;
+}
+
+function Preview() {
+  // No SSR for the live preview
+  // See https://github.com/facebook/docusaurus/issues/5747
+  return (
+    <BrowserOnly fallback={<LivePreviewLoader />}>
+      {() => (
+        <>
+          <ErrorBoundary
+            fallback={(params) => (
+              <ErrorBoundaryErrorMessageFallback {...params} />
+            )}
+          >
+            <LivePreview />
+          </ErrorBoundary>
+          <LiveError />
+        </>
+      )}
+    </BrowserOnly>
+  );
+}
+
+function ResultWithHeader() {
+  return (
+    <>
+      <Header>
+        <Translate
+          id="theme.Playground.result"
+          description="The result label of the live codeblocks"
+        >
+          Result
+        </Translate>
+      </Header>
+      {/* https://github.com/facebook/docusaurus/issues/5747 */}
+      <div className={styles.playgroundPreview}>
+        <Preview />
+      </div>
+    </>
+  );
+}
+
+function ThemedLiveEditor() {
+  const isBrowser = useIsBrowser();
+  return (
+    <LiveEditor
+      // We force remount the editor on hydration,
+      // otherwise dark prism theme is not applied
+      key={String(isBrowser)}
+      className={styles.playgroundEditor}
+    />
+  );
+}
+
+function EditorWithHeader() {
+  return (
+    <>
+      <Header>
+        <Translate
+          id="theme.Playground.liveEditor"
+          description="The live editor label of the live codeblocks"
+        >
+          Live Editor
+        </Translate>
+      </Header>
+      <ThemedLiveEditor />
+    </>
+  );
+}
+
+// this should rather be a stable function
+// see https://github.com/facebook/docusaurus/issues/9630#issuecomment-1855682643
+// Removed the ; at the end of the line
+// see: https://github.com/FormidableLabs/react-live/issues/403
+const DEFAULT_TRANSFORM_CODE = (code: string) => `${code}`;
+
+export default function Playground({
+  children,
+  transformCode,
+  ...props
+}: Props): JSX.Element {
+  const {
+    siteConfig: { themeConfig },
+  } = useDocusaurusContext();
+  const {
+    liveCodeBlock: { playgroundPosition },
+  } = themeConfig as ThemeConfig;
+  const prismTheme = usePrismTheme();
+
+  const noInline = props.metastring?.includes("noInline") ?? false;
+
+  return (
+    <div className={styles.playgroundContainer}>
+      <LiveProvider
+        code={children?.replace(/\n$/, "")}
+        noInline={noInline}
+        transformCode={transformCode ?? DEFAULT_TRANSFORM_CODE}
+        theme={prismTheme}
+        {...props}
+      >
+        {playgroundPosition === "top" ? (
+          <>
+            <ResultWithHeader />
+            <EditorWithHeader />
+          </>
+        ) : (
+          <>
+            <EditorWithHeader />
+            <ResultWithHeader />
+          </>
+        )}
+      </LiveProvider>
+    </div>
+  );
+}
