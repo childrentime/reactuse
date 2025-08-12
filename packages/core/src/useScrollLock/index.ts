3d89481160da9e3dcc2a2b804de555fce9560aff
Berlin <58261676+childrentime@users.noreply.github.com>
2025-08-12 13:59:30 +0800

    fix(useScrollLock): ios (#159)
    * fix(useScrollLock): ios
    
    * chore: release v6.0.6

--- packages/core/src/useScrollLock/index.ts
+++ packages/core/src/useScrollLock/index.ts
@@ -4,16 +4,41 @@ import { isIOS } from '../utils/is'
 import { useEvent } from '../useEvent'
 import type { UseScrollLock } from './interface'
 
-function preventDefault(rawEvent: TouchEvent): boolean {
-  const e = rawEvent || window.event
-  // Do not prevent if the event has more than one touch (usually meaning this is a multi touch gesture like pinch to zoom).
-  if (e.touches.length > 1) {
+function checkOverflowScroll(ele: Element): boolean {
+  const style = window.getComputedStyle(ele)
+  if (
+    style.overflowX === 'scroll'
+    || style.overflowY === 'scroll'
+    || (style.overflowX === 'auto' && ele.clientWidth < ele.scrollWidth)
+    || (style.overflowY === 'auto' && ele.clientHeight < ele.scrollHeight)
+  ) {
     return true
   }
+  else {
+    const parent = ele.parentNode as Element
 
-  if (e.preventDefault) {
-    e.preventDefault()
+    if (!parent || parent.tagName === 'BODY')
+      return false
+
+    return checkOverflowScroll(parent)
   }
+}
+
+function preventDefault(rawEvent: TouchEvent): boolean {
+  const e = rawEvent || window.event
+
+  const _target = e.target as Element
+
+  // Do not prevent if element or parentNodes have overflow: scroll set.
+  if (checkOverflowScroll(_target))
+    return false
+
+  // Do not prevent if the event has more than one touch (usually meaning this is a multi touch gesture like pinch to zoom).
+  if (e.touches.length > 1)
+    return true
+
+  if (e.preventDefault)
+    e.preventDefault()
 
   return false
 }
